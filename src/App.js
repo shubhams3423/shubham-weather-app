8c567c6f2d6114b87a73039000451cee3517a848
shubhams3423 <shubham1844s@gmail.com>
2023-08-05 11:00:41 +0530

    WeatherFetcher component created and also added toastify for then and catch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
-
+import WeatherFetcher from './component/WeatherFetcher';
 function App() {
-  fetch("https://api.weatherapi.com/v1/current.json?key=5bca6bccf3ce4c41a8f15340232807&q=pune")
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error))
-
   return (
     <div className="App">
       <div style={{ fontSize: "5rem" }}>My Weather app - Javascript project</div>
+      <WeatherFetcher />
+
     </div>
-  );
+  )
 }
 
 export default App;
