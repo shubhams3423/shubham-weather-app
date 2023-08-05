530f8079e5f52916ddc505408503d1afeae75c7d
shubhams3423 <shubham1844s@gmail.com>
2023-08-05 08:46:58 +0530

    Weather data fetched

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  fetch("https://api.weatherapi.com/v1/current.json?key=5bca6bccf3ce4c41a8f15340232807&q=pune")
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error))
+
   return (
     <div className="App">
       <div style={{ fontSize: "5rem" }}>My Weather app - Javascript project</div>
